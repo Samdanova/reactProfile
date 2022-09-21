ea3677fc25c94f063d7008dc1a206adcbd6ee6bc
Samdanova <samdanova123@gmail.com>
2022-09-21 16:28:24 +0300

    searching

--- src/assets/components/Auth.tsx
+++ src/assets/components/Auth.tsx
@@ -1,15 +1,26 @@
 import { Navigate } from 'react-router-dom';
+import contacts from '../../../contacts.json'
+import Contacts from './Contacts';
 
 interface IAuthProps {
     children: React.ReactNode;
     loggedIn: boolean;
 }
 
+interface iContacts {
+    id: number;
+    name: string;
+    mobile: number;
+    email: string
+}
+
 function Auth({ children, loggedIn }: IAuthProps) {
     if (!loggedIn) {
         return <Navigate to="/login" />;
     }
-    return <>{children}</>;
+    return <>
+        {children}
+    </>;
 };
 
 export default Auth;
--- src/assets/components/Contacts.tsx
+++ src/assets/components/Contacts.tsx
@@ -1,7 +1,11 @@
 import { inject, observer } from 'mobx-react';
-import { Button } from 'antd';
-import { Space, Table, Tag } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
+import { SearchOutlined } from '@ant-design/icons';
+import type { InputRef } from 'antd';
+import { Button, Input, Space, Table } from 'antd';
+import type { ColumnsType, ColumnType } from 'antd/es/table';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
+import React, { useRef, useState } from 'react';
+import Highlighter from "react-highlight-words";
 
 interface IContactProps {
     getContacts: () => void;
@@ -20,22 +24,116 @@ interface ContactsType {
 
 function Contacts({ getContacts, userContacts }: IContactProps) {
 
+    type DataIndex = keyof ContactsType;
+
+
+
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef<InputRef>(null);
+
+
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: (param?: FilterConfirmProps) => void,
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<ContactsType> => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes((value as string).toLowerCase()),
+        onFilterDropdownOpenChange: visible => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: text =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
     const columns: ColumnsType<ContactsType> = [
         {
             title: 'Имя',
             dataIndex: 'name',
             key: 'name',
             render: text => <a>{text}</a>,
+            ...getColumnSearchProps('name'),
         },
         {
             title: 'Номер',
             dataIndex: 'mobile',
             key: 'mobile',
+            ...getColumnSearchProps('mobile'),
         },
         {
             title: 'E-mail',
             dataIndex: 'email',
             key: 'email',
+            ...getColumnSearchProps('email')
         },
         {
             title: 'Внести изменение',
